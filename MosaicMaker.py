65a98ff4a015f9f2bcf024842352af1708425d2a
Maxamilian Demian <Maxoplata@users.noreply.github.com>
2021-09-21 20:18:51 -0700

    Description examples cleanup

--- MosaicMaker.py
+++ MosaicMaker.py
@@ -4,7 +4,7 @@ MosaicMaker.py
 
 Creates a mosaic image png
 usage: python MosaicMaker.py TILE_SIZE "input url/filepath" "output png file"
-example: python MosaicMaker.py 20 "https://raw.githubusercontent.com/Maxoplata/misc/master/MosaicMaker/_readmeAssets/sampleInput.jpg" "./mosaic.png"
+example: python MosaicMaker.py 20 "https://raw.githubusercontent.com/Maxoplata/MosaicMaker/main/_readmeAssets/sampleInput.jpg" "./mosaic.png"
 example: python MosaicMaker.py 20 "./sampleInput.jpg" "./mosaic.png"
 
 https://www.maxodev.org
